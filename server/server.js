0eef2a78901179c6c5dac8affb560a536536f379
Kedirech <brackcarmony@gmail.com>
2017-02-07 16:56:04 -0700

    Clearing Server.js git status

--- server/server.js
+++ server/server.js
@@ -1,27 +1,15 @@
 // Default Express App
-var express = require('express');
-var app = module.exports = express();
-var bodyParser = require('body-parser');
-var port = 8080;
 
 // Connect Massive
-var massive = require('massive');
-var connectionString = "postgres://postgres:postgres@localhost/massive";
-var massiveInstance = massive.connectSync({connectionString:connectionString})
 
-app.set('db', massiveInstance);
-var productCtrl = require('./controllers/productCtrl');
-var cartCtrl = require('./controllers/cartCtrl');
+// Controllers
 
-app.use(bodyParser.json());
+// Body Parser
 
-app.get('/api/products', productCtrl.getAll)
-app.get('/api/products/:productId', productCtrl.getOne)
-app.post('/api/products', productCtrl.create)
-app.put('/api/products/:productId', productCtrl.update)
-app.delete('/api/products/:productId', productCtrl.delete)
+// Hero Endpoints
 
+// Power Endpoints
 
-app.listen(port, function(){
-  console.log("listening on port" + port)
-})
+// Hero_Power Endpoints
+
+// Listen
